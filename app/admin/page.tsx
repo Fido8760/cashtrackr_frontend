3f2e6aad2b0f749823219b672a8a52c62944b8b4
Fidel <fidel050987@gmail.com>
2025-03-05 09:55:20 -0600

    frontend ya con presupuesto y y gastos, se inicia la edicion y la eliminación de gastos

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,7 +1,36 @@
 
+import BudgetMenu from "@/components/budgets/BudgetMenu";
+import DeleteBudgetModal from "@/components/budgets/DeleteBudgetModal";
+import getToken from "@/src/auth/token";
+import { BudgetsAPIResponseSchema } from "@/src/schemas";
+import { formatCurrency, formatDate } from "@/src/utils";
+import { Metadata } from "next";
 import Link from "next/link";
 
+export const metadata : Metadata = {
+    title: 'CashTrackr - Panel de Administración',
+    description: 'CashTrackr - Panel de Administración'
+}
+
+async function getUserBudgets() {
+    const token = getToken()
+    const url = `${process.env.API_URL}/budgets`
+
+    const req = await fetch(url, {
+        headers: {
+            'Authorization': `Bearer ${token}`
+        }
+    })
+
+    const json = await req.json()
+    const budgets = BudgetsAPIResponseSchema.parse(json)
+    return budgets 
+}
+
 export default async function AdminPage() {
+
+    const budgets = await getUserBudgets()
+
 
     return (
         <>
@@ -19,6 +48,52 @@ export default async function AdminPage() {
                 Crear Presupuesto
             </Link>
             </div>
+
+            {budgets.length ? (
+                <>
+                    <ul role="list" className="divide-y divide-gray-300 border shadow-lg mt-10 ">
+                        {budgets.map((budget) => (
+                        <li key={budget.id} className="flex justify-between gap-x-6 p-5 ">
+                            <div className="flex min-w-0 gap-x-4">
+                            <div className="min-w-0 flex-auto space-y-2">
+                                <p className="text-sm font-semibold leading-6 text-gray-900">
+                                    <Link 
+                                        href={`/admin/budget/${budget.id}`}
+                                        className="cursor-pointer hover:underline text-2xl font-bold"
+                                    >{budget.name}</Link>
+                                </p>
+                                <p className="text-xl font-bold text-amber-500">
+                                    {formatCurrency(+budget.amount)}
+                                </p>
+                                <p className='text-gray-500  text-sm'>
+                                    Última Actualización: {''}
+                                    <span className=" font-bold uppercase">{formatDate(budget.updatedAt)}</span>
+                                </p>
+                            </div>
+                            </div>
+                            <div className="flex shrink-0 items-center gap-x-6">
+                                <BudgetMenu 
+                                    budgetId={budget.id}
+                                />
+                            </div>
+                        </li>
+                        ))}
+                    </ul>
+
+                    <DeleteBudgetModal />
+                
+                </>
+            ) : (
+                <p className=" text-center py-20">No hay presupuestos aún {''}
+                    <Link
+                        className=" text-purple-950 font-bold"
+                        href={'/admin/budget/new'}
+                    >
+                        comienza creando uno
+                    
+                    </Link>
+                </p>
+            )}
     
         </>        
 
